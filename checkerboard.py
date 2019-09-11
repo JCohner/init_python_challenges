747ae0dc6d99842b465c574e62d29736c364db01
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-11 15:22:41 -0500

    got click detection woring

--- checkerboard.py
+++ checkerboard.py
@@ -2,9 +2,16 @@ from __future__  import print_function
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.plot()
-x_axis = np.arange(7)
-y_axis = np.arange(6)
+fig = plt.figure()
+#ax = fig.add_subplot(111)
+
+def onclick(event):
+    print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+          ('double' if event.dblclick else 'single', event.button,
+           event.x, event.y, event.xdata, event.ydata))
+
+x_axis = np.arange(7 + 1)
+y_axis = np.arange(6 + 1)
 
 for x in range(len(x_axis)):
     line = np.zeros(len(y_axis)) + x
@@ -13,6 +20,8 @@ for x in range(len(x_axis)):
 for y in range(len(y_axis)):
     line = np.zeros(len(x_axis)) + y
     plt.plot(np.arange(len(x_axis)), line, 'k')
-
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
+
+
 
