e2f2433ec3565573b65836d0ab4ff62b7d7fcc91
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-11 15:49:35 -0500

    got click creating elements in dataframe

--- checkerboard.py
+++ checkerboard.py
@@ -2,22 +2,22 @@ from __future__  import print_function
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import pandas as pd
 
 fig = plt.figure()
 #ax = fig.add_subplot(111)
 
 def onclick(event):
-    print(event.xdata)
-
-
-    print(int(math.floor(event.xdata)))
-    
-    #print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-          #('double' if event.dblclick else 'single', event.button,
-          # event.x, event.y, event.xdata, event.ydata))
-
+    #print(event.xdata)
+    col = int(math.floor(event.xdata))
+    print(df.iloc[-1, col])
+    if (df.iloc[-1, col] == 0):
+        df.iloc[-1, col] = 1
+    print(df)
 x_axis = np.arange(7 + 1)
 y_axis = np.arange(6 + 1)
+
+df = pd.DataFrame(np.zeros((7,6))).astype('int32')
 
 for x in range(len(x_axis)):
     line = np.zeros(len(y_axis)) + x
