ea9cd4ec49d70f9fea093f455493dd65f9670504
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-11 16:14:21 -0500

    current working state

--- checkerboard.py
+++ checkerboard.py
@@ -7,15 +7,24 @@ import pandas as pd
 fig = plt.figure()
 #ax = fig.add_subplot(111)
 
+players = ['p1', 'p2']
+turn = 0
 def onclick(event):
     #print(event.xdata)
     col = int(math.floor(event.xdata))
     print(df.iloc[-1, col])
     check_df(col, -1)
     print(df)
+
 def check_df(col, row):
+    if (active_player == players[0]):
+        marker = 1
+    elif (active_player == players[1]):
+        marker = -1
+    
     if(df.iloc[row, col] == 0):
-        df.iloc[row, col] = 1
+        df.iloc[row, col] = marker
+        active_player = players[(turn) + 1 % 2]
     elif (row < -6):
         print("you cant place there")
     else:
@@ -23,6 +32,8 @@ def check_df(col, row):
 x_axis = np.arange(7 + 1)
 y_axis = np.arange(6 + 1)
 
+active_player = players[turn]
+print(active_player)
 df = pd.DataFrame(np.zeros((8,7))).astype('int32')
 
 for x in range(len(x_axis)):
