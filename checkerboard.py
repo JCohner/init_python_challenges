e30d2525620a196ba882a475b24444f81fa46b60
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-12 12:12:42 -0500

    matplotlib now runs in interactive mode allows for much cleaner state cycle

--- checkerboard.py
+++ checkerboard.py
@@ -5,153 +5,91 @@ import math
 import pandas as pd
 from termcolor import cprint
 
+plt.ion()  
 
 #ax = fig.add_subplot(111)
 class Game():
     def check_df(self, col, row):
-        if (self.active_player == self.players[0]):
-            marker = 1
-        elif (self.active_player == self.players[1]):
-            marker = -1
-        
         if(self.df.iloc[row, col] == 0):
-            self.df.iloc[row, col] = marker
-            self.turn = (self.turn + 1) % 2
-            self.active_player = self.players[self.turn]
-            print(self.active_player)
-            self.make_circle(col, row, marker)
+            self.df.iloc[row, col] = self.active_player[2]
+            self.make_circle(col, row)
+            self.win_check(col, row)
+            self.next_player() 
         elif (row < -7):
             print("you cant place there")
+            self.prompt_player("you can't place there!", 'k')
         else:
-            self.check_df(col, row - 1)
+            self.check_df(col, row - 1) #some beautiful recursion if i say so myself
 
-    def make_circle(self, col, row, marker):
-        
+    def make_circle(self, col, row):
         pos = (col + 0.5 ,abs(row) - 0.5)
-        #print(pos)
-        if (marker == 1):
-            color = 'r'
-        elif (marker == -1):
-            color = 'b'
-        circle = plt.Circle(pos, 0.4, color = color)
+        circle = plt.Circle(pos, 0.4, color = self.active_player[1])
         self.ax.add_artist(circle)
-        self.win_check(col, row)
         plt.show()
 
    
     def win_check(self, col, row):
-        #col_win()
-        win_col_1 = (self.df.iloc[:, col] == 1)
-        win_col_2 = (self.df.iloc[:, col] == -1)
+        #col win check
+        win_col = (self.df.iloc[:, col] == self.active_player[2])
+        win_col_counts = win_col.value_counts()
 
-        win_col_1_counts = win_col_1.value_counts()
-        win_col_2_counts = win_col_2.value_counts()
+        if (len(win_col_counts.index) > 1):
+            num_pieces = win_col_counts.loc[True]
+            print(self.active_player[0] + " has " + str(num_pieces) + " pieces in this col")
 
-        if (len(win_col_1_counts.index) > 1):
-            num_pieces = win_col_1_counts.loc[True]
-            print("player 1 has " + str(num_pieces) + " pieces in this col")
-            if (num_pieces >= 4):
-                print(win_col_1)
+            if (num_pieces > 3):
                 seq = 0
                 x = 0
-                while x < len(win_col_1) and seq < 4 :
-                    if (win_col_1[x] == True):
+                print("checking if they're seq")
+                while x < len(win_col) and seq < 4 :
+                    if (win_col[x] == True):
                         seq = seq + 1
                     else:
                         seq = 0
                     x = x + 1
                 if (seq >= 4):
-                    self.game_state = 1
-                    cprint("player 1 wins!", 'red')
+                    self.game_state = 1 #TODO: have this stop the game, maybe offer reset
+                    self.win_print()
 
-        if (len(win_col_2_counts.index) > 1):
-            num_pieces = win_col_2_counts.loc[True]
-            print("player 2 has " + str(num_pieces) + " pieces in this col")
-            if (num_pieces >= 4):
-                print(win_col_2)
+        #row win check
+        win_row = (self.df.iloc[row, :] == self.active_player[2])
+        win_row_counts = win_row.value_counts()
+
+        if (len(win_row_counts.index) > 1):
+            num_pieces = win_row_counts.loc[True]
+            print(self.active_player[0] + " has " + str(num_pieces) + " pieces in this row")
+
+            if (num_pieces > 3):
                 seq = 0
                 x = 0
-                while x < len(win_col_2) and seq < 4 :
-                    if (win_col_2[x] == True):
+
+                while x < len(win_row) and seq < 4 :
+                    if (win_row[x] == True):
                         seq = seq + 1
                     else:
                         seq = 0
                     x = x + 1
                 if (seq >= 4):
-                    self.game_state = -1
-                    cprint("player 2 wins!", 'blue')      
-
-        #row_win()
-        win_row_1 = (self.df.iloc[row, :] == 1)
-        win_row_2 = (self.df.iloc[row, :] == -1)
-
-        win_row_1_counts = win_row_1.value_counts()
-        win_row_2_counts = win_row_2.value_counts()
-
-        if (len(win_row_1_counts.index) > 1):
-            num_pieces = win_row_1_counts.loc[True]
-            print("player 1 has " + str(num_pieces) + " pieces in this row")
-            if (num_pieces >= 4):
-                print(win_row_1)
-                seq = 0
-                x = 0
-                while x < len(win_row_1) and seq < 4 :
-                    if (win_row_1[x] == True):
-                        seq = seq + 1
-                    else:
-                        seq = 0
-                    x = x + 1
-                if (seq >= 4):
-                    self.game_state = 1
-                    cprint("player 1 wins!", 'red')
-
-        if (len(win_row_2_counts.index) > 1):
-            num_pieces = win_row_2_counts.loc[True]
-            print("player 2 has " + str(num_pieces) + " pieces in this row")
-            if (num_pieces >= 4):
-                print(win_row_2)
-                seq = 0
-                x = 0
-                while x < len(win_row_2) and seq < 4 :
-                    if (win_row_2[x] == True):
-                        seq = seq + 1
-                    else:
-                        seq = 0
-                    x = x + 1
-                if (seq >= 4):
-                    self.game_state = -1
-                    cprint("player 2 wins!", 'blue')      
-        #diag_win()
-        if (self.active_player == self.players[0]):
-            marker = -1 #player 2 is actually going, i must be updating active player at worong point TODO: fix this
-        elif (self.active_player == self.players[1]):
-            marker = 1
-
-        total_seq_1 = self.pos_diag_win_check(col, row, [], marker)
+                    self.game_state = 1 #TODO: have this stop the game, maybe offer reset
+                    self.win_print()
+          
+        #diag win check
+        total_seq_1 = self.pos_diag_win_check(col, row, [], self.active_player[2])
         print("positive slope total " + str(total_seq_1))
 
-        total_seq_2 = self.neg_diag_win_check(col, row, [], marker)
+        total_seq_2 = self.neg_diag_win_check(col, row, [], self.active_player[2])
         print("neg slope total " + str(total_seq_2))
 
         if (total_seq_1 >= 4 or total_seq_2 >= 4):
-            if (self.active_player == self.players[0]):
-                cprint("player 2 wins!", 'blue')
-                self.game_state = -1
-            elif (self.active_player == self.players[1]):
-                cprint("player 1 wins!", 'red')
-                self.game_state = 1
-        self.win_print()
+            self.game_state = self.active_player[2]
+            self.win_print()       
 
     def win_print(self):
-        if(self.game_state == 1):
-            plt.text(.75, 3, "Player 1 Wins!", fontsize = 40, color = 'r')
-            plt.show()
-        elif (self.game_state == -1):
-            plt.text(.75, 3, "Player 2 Wins!", fontsize = 40, color = 'b')
-            plt.show()
+        s = self.active_player[0] + " wins!"
+        plt.text(.75, 3, s, fontsize = 40, color = self.active_player[1])
+        plt.pause(5)
 
     def pos_diag_win_check(self, col, row, checked, player):
-        print((row, col))
         pos_slope_neighbors = []
         
         for x in range(-1,2,2):
@@ -210,42 +148,59 @@ class Game():
         print("neg slope recurse sum " + str(recurse_sum))
         return recurse_sum
 
+    def prompt_player(self, s, c):
+        plt.text(0, -0.25 , s, color = c)
+
+    def next_player(self):
+        self.turn = (self.turn + 1) % 2 #increments between 0 and 1 indicating player 1 and 2 respectively
+        self.active_player = [self.player_names[self.turn], self.player_colors[self.turn], self.player_markers[self.turn]]
+        self.prompt_player(self.active_player[0] + " your move!", self.active_player[1])
+        print(self.active_player)
+
     def onclick(self, event):
-            #print(self.active_player)
             col = int(math.floor(event.xdata))
-            
-            print(self.df.iloc[-1, col])
             self.check_df(col, -1)
             print(self.df)
 
     def __init__(self):
-        self.num_rows = 7 + 1
-        self.num_columns = 6 +1
-        self.df = pd.DataFrame(np.zeros((self.num_rows,self.num_columns))).astype('int32')
-        self.game_state = 0
-        print(self.df)
-        
+        #Set number of rows and columns for game board
+        self.num_rows = 7 
+        self.num_columns = 6
 
+        #intitalize dataframe that will keep track of our game
+        self.df = pd.DataFrame(np.zeros((self.num_rows,self.num_columns))).astype('int32')
+        
+        #create the matplot lib figure and populate it with tile
+             
         fig, self.ax = plt.subplots()
-        x_axis = np.arange(self.num_rows)
-        y_axis = np.arange(self.num_columns)
+        x_axis = np.arange(self.num_rows + 1) #+1 is needed for correct tiling
+        y_axis = np.arange(self.num_columns + 1) #+1 is needed for correct tiling
         for x in range(len(x_axis)):
             line = np.zeros(len(y_axis)) + x
             plt.plot(line, np.arange(len(y_axis)), 'k')
-
         for y in range(len(y_axis)):
             line = np.zeros(len(x_axis)) + y
             plt.plot(np.arange(len(x_axis)), line, 'k')
-        cid = fig.canvas.mpl_connect('button_press_event', self.onclick)
-        
 
-        self.players = ['p1', 'p2']
+        #links on click events from figure to onclick function
+        cid = fig.canvas.mpl_connect('button_press_event', self.onclick)
+        #game state variable
+        self.game_state = 0
+        self.player_names = ['p1', 'p2']
+        self.player_colors = ['r', 'b']
+        self.player_markers = [1, -1]
         self.turn = 0
-        self.active_player = self.players[self.turn]
-        print(self.active_player)
-        plt.show()
+        self.active_player = [self.player_names[self.turn], self.player_colors[self.turn], self.player_markers[self.turn]] #provides, name, color and marker for active player
+      
+        self.prompt_player(self.active_player[0] + " your move!", self.active_player[1])
+
+# class active_player():
+#     def __init__(self):
+
 
 
 g = Game()
+while(g.game_state == 0):
+    plt.pause(0.1)
 
 
